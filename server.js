a0d2ff3dce2377be525211ff7238f3d136a5383c
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2023-05-29 17:48:44 +0200

    fix cors + add !NODE_ENV throw

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const routes = require("./api/routes")
 
+if (!process.env.NODE_ENV) throw new Error('you need to define process.env.NODE_ENV')
 require('dotenv').config({
     path: `./config/config.${process.env.NODE_ENV}.env`
 });
@@ -22,13 +23,20 @@ const port = PORT || 8080;
 const host = HOST || 'localhost'
 const app = express();
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 const corsOptions = {
     origin(origin, callback) {
-        if (CORSWHITELIST.includes(origin) || !origin) {
+        if (JSON.parse(CORSWHITELIST).includes(origin) || !origin) {
             callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
+            return
         }
+        callback(new Error('Not allowed by CORS'));
     },
 };
 app.use(cors(corsOptions));
@@ -42,8 +50,8 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(port, async () => {
-    console.log(`Server is running on port: ${port}`);
+app.listen(port, host, async () => {
+    console.log(`Server is running on: ${host}:${port}`);
 
     // mongoose instance
     mongoose.Promise = global.Promise;
