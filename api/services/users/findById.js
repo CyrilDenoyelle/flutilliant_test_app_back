beaf5407fa8a59829c5a1a8bdd76b94e4046a3b5
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2023-05-31 18:25:28 +0200

    add find by id

---
+++ api/services/users/findById.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const User = require('../../models/userModel');
+
+// const ObjectId = require("mongodb").ObjectId; ?????
+
+module.exports = async (id) => {
+    try {
+        const user = await User.findOne({ _id: id }).exec();
+
+        if (!user) {
+            throw new Error('User not found')
+        }
+        return user;
+    } catch (error) {
+        console.error(`[service] [${__dirname}]`, error)
+        throw error
+    }
+
+};
