d4de672fcf71469fcfc80a73dbd256c5f1c6b8f7
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2023-06-02 16:56:01 +0200

    update eslintrc to accept jest and mocha env var

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
         commonjs: true,
         es2021: true,
         node: true,
+        jest: true,
+        mocha: true,
     },
     extends: 'airbnb-base',
     parserOptions: {
